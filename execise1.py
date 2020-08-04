091cb7ce912f4d109df949328b26832d5f9f7295
watchirawit <68271994+watchirawit@users.noreply.github.com>
2020-08-04 07:47:01 +0700

    work_edit

--- execise1.py
+++ execise1.py
@@ -2,9 +2,10 @@ score1 = int(input('enter the score for test1:  '))
 score2 = int(input('enter the score for test2:  '))
 score3 = int(input('enter the score for test3:  '))
 score_all = (score1 + score2 + score3) / 3 
+print("\nthe average score is",score_all) 
 if score_all >= 95 : 
-    print("the average score is",score_all,"\ncongratulations!\nthat is a great average")  
+   print("congratulations!\nthat is a great average")  
     
-if score_all < 95 :
-    print("the average score is",score_all)
+
+
  
