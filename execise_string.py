a2031b7e799493621536ac852737d64fb9f7978b
watchirawit <68271994+watchirawit@users.noreply.github.com>
2020-08-18 10:18:23 +0700

    string_test

---
+++ execise_string.py
@@ -0,0 +1,4 @@
+First_name = str(input("Enter your frist name: "))
+Last_name = str(input("Enter your last name:  "))
+student_id = str(input("Enter your student ID number: "))
+print('Your system login nmae is:\n',First_name[0:3]+Last_name[0:3]+student_id[-3]+student_id[-2]+student_id[-1])
